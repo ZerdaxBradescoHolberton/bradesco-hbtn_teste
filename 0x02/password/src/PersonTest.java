4c57bba4f827aeb45013739b4db69e01ddd8d5b5
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-10-04 12:06:53 -0300

    [Projeto 0x02/password] - Implementação 1

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PersonTest {
 
     private static Person person;
@@ -17,54 +18,46 @@ public class PersonTest {
         person.setPassword("");
     }
 
-    @Test
-    public void check_user_valid() {
-        List.of("PaulMcCartney2", "NeilArms2").forEach(s -> {
-            person.setUsername(s);
-            assertTrue(person.checkUser());
-        });
+    @ParameterizedTest
+    @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
+    public void check_user_valid(String s) {
+        person.setUsername(s);
+        assertTrue(person.checkUser());
     }
 
-    @Test
-    public void check_user_not_valid() {
-        List.of("Paul#McCartney", "Neil@Arms").forEach(s -> {
-            person.setUsername(s);
-            assertFalse(person.checkUser());
-        });
+    @ParameterizedTest
+    @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
+    public void check_user_not_valid(String s) {
+        person.setUsername(s);
+        assertFalse(person.checkUser());
     }
 
-    @Test
-    public void does_not_have_letters() {
-        List.of("123456789", "#$%1234").forEach(s -> {
-            person.setPassword(s);
-            assertFalse(person.checkPassword());
-        });
+    @ParameterizedTest
+    @ValueSource(strings = {"123456789", "#$%1234"})
+    public void does_not_have_letters(String s) {
+        person.setPassword(s);
+        assertFalse(person.checkPassword());
     }
 
-    @Test
-    public void does_not_have_numbers() {
-        List.of("Abcabcdefgh@", "#hbtn@%tc").forEach(s -> {
-            person.setPassword(s);
-            assertFalse(person.checkPassword());
-        });
+    @ParameterizedTest
+    @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
+    public void does_not_have_numbers(String s) {
+        person.setPassword(s);
+        assertFalse(person.checkPassword());
     }
 
-    @Test
-    public void does_not_have_eight_chars() {
-        List.of("Abc@123", "12$@hbt").forEach(s -> {
-            person.setPassword(s);
-            assertFalse(person.checkPassword());
-        });
-
+    @ParameterizedTest
+    @ValueSource(strings = {"Abc@123", "12$@hbt"})
+    public void does_not_have_eight_chars(String s) {
+        person.setPassword(s);
+        assertFalse(person.checkPassword());
     }
 
-    @Test
-    public void check_password_valid() {
-        List.of("abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123").forEach(s -> {
-            person.setPassword(s);
-            assertTrue(person.checkPassword());
-        });
-
+    @ParameterizedTest
+    @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
+    public void check_password_valid(String s) {
+        person.setPassword(s);
+        assertTrue(person.checkPassword());
     }
 
 }
